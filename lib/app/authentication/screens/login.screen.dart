9f5bcb23154a537a0a11930aab4faff89ff76b54
Andre Lehnert <andre.lehnert@exxcellent.de>
2020-07-26 15:32:51 +0200

    Refactor login screen

--- lib/app/authentication/screens/login.screen.dart
+++ lib/app/authentication/screens/login.screen.dart
@@ -17,53 +17,63 @@ class LogInScreen extends StatelessWidget {
   }
 
   Widget _buildContent(context, state) {
-
     TextEditingController userTextField = TextEditingController(text: 'ligadefault');
     TextEditingController passwordTextField = TextEditingController(text: 'user');
 
     return FormCard(
-            children: [
-              Headline(Routes.LOG_IN.label),
-              TextFormField(
-                controller: userTextField,
-                decoration: InputDecoration(
-                  hintText: 'E-Mail',
-                ),
-                minLines: 1,
-                keyboardType: TextInputType.emailAddress,
-              ),
-              TextFormField(
-                controller: passwordTextField,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                ),
-                obscureText: true,
-                minLines: 1,
-                autofocus: false,
-                keyboardType: TextInputType.text,
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              PrimaryButton(
-                child: Text('LOGIN'),
-                onPressed: () async {
-                  var userName = userTextField.text;
-                  var password = passwordTextField.text;
+        children: [
+          Headline(Routes.LOG_IN.label),
+          _userNameTextfield(context, userTextField),
+          _passwordTextfield(context, passwordTextField),
+          SizedBox(
+            height: 24,
+          ),
+          _submitButton(context, userTextField, passwordTextField)
+        ],
+    );
+  }
 
-                  debugPrint('userName: $userName');
-                  debugPrint('password: $password');
+  TextFormField _userNameTextfield(BuildContext context, controller) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: 'E-Mail',
+      ),
+      minLines: 1,
+      keyboardType: TextInputType.emailAddress,
+    );
+  }
 
-                  try {
-                    AuthenticationBloc.of(context).add(Login(userName, password));
+  TextFormField _passwordTextfield(BuildContext context, controller) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: 'Password',
+      ),
+      obscureText: true,
+      minLines: 1,
+      autofocus: false,
+      keyboardType: TextInputType.text,
+    );
+  }
 
-                    Routes.navigateTo(context, Routes.HOME);
-                  } catch (e) {
-                    print('Error: $e');
-                  }
-                },
-              )
-            ],
-        );
+  PrimaryButton _submitButton(context, TextEditingController userTextField, TextEditingController passwordTextField) {
+    return PrimaryButton(
+      child: Text('LOGIN'),
+      onPressed: () async {
+        var userName = userTextField.text;
+        var password = passwordTextField.text;
+
+        debugPrint('userName: $userName');
+        debugPrint('password: $password');
+
+        try {
+          AuthenticationBloc.of(context).add(Login(userName, password));
+          Routes.navigateBack(context);
+        } catch (e) {
+          print('Error: $e');
+        }
+      },
+    );
   }
 }
